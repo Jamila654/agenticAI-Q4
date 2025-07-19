e0b80acb3107308bbe8895fb5ef26d3edd12b6a9
Jamila654 <136564835+Jamila654@users.noreply.github.com>
2025-07-19 17:09:21 +0500

    done

--- assignments/class_ass_context_handoff_tools/app.py
+++ assignments/class_ass_context_handoff_tools/app.py
@@ -19,7 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://mathsorpythonteacher.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
